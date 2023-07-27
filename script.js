d9dc0965a835e70b96c88c3ac46ddd371397ac4a
Emily Loan <emily.loan@outlook.com>
2023-07-27 19:37:12 +0100

    leisure & restaurants swiper cards

--- script.js
+++ script.js
@@ -17,4 +17,136 @@ buttons.forEach(button => {
   })
 })
 
+// For the restaurants and activities image slider
 
+const carousel = document.querySelector(".slider");
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false, startX, startScrollLeft;
+
+// Get the number of cards that can fit in the carousel at once
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+// Inserts copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+  carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+// Inserts copies of the last few cards to end of carousel for infinite scrolling
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+  carousel.insertAdjacentHTML("beforeEnd", card.outerHTML);
+});
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+  })
+})
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  // Records the initial cursor and scroll position of the carousel
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+  if(!isDragging) return; // if isDragging is false return from here
+  // Updates the scroll position of the carousel based on the cursor movement
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+}
+
+const infiniteScroll = () => {
+  // If the carousel is at the beginning, scroll to the end
+  if(carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - ( 2 * carousel.offsetWidth);
+    carousel.classList.remove("no-transition");
+  } 
+  // If the carousel is at the end, scroll to the beginning
+  else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+  }
+}
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+
+//Second carousel
+
+const carousel2 = document.querySelector(".slider2");
+const arrowBtns2 = document.querySelectorAll(".wrapper2 i");
+const firstCardWidth2 = carousel2.querySelector(".card2").offsetWidth;
+const carouselChildrens2 = [...carousel2.children];
+
+let isDragging2 = false, startX2, startScrollLeft2;
+
+// Get the number of cards that can fit in the carousel at once
+let cardPerView2 = Math.round(carousel2.offsetWidth / firstCardWidth2);
+
+// Inserts copies of the last few cards to beginning of carousel for infinite scrolling
+carouselChildrens2.slice(-cardPerView2).reverse().forEach(card => {
+  carousel2.insertAdjacentHTML("afterbegin", card.outerHTML);
+});
+
+// Inserts copies of the last few cards to end of carousel for infinite scrolling
+carouselChildrens2.slice(0, cardPerView2).forEach(card => {
+  carousel2.insertAdjacentHTML("beforeEnd", card.outerHTML);
+});
+
+// Add event listeners for the arrow buttons to scroll the carousel left and right
+arrowBtns2.forEach(btn => {
+  btn.addEventListener("click", () => {
+    carousel2.scrollLeft += btn.id === "left" ? -firstCardWidth2 : firstCardWidth2;
+  })
+})
+
+const dragStart2 = (e) => {
+  isDragging = true;
+  carousel2.classList.add("dragging2");
+  // Records the initial cursor and scroll position of the carousel
+  startX2 = e.pageX;
+  startScrollLeft2 = carousel2.scrollLeft;
+}
+
+const dragging2 = (e) => {
+  if(!isDragging) return; // if isDragging is false return from here
+  // Updates the scroll position of the carousel based on the cursor movement
+  carousel2.scrollLeft = startScrollLeft2 - (e.pageX - startX2);
+}
+
+const dragStop2 = () => {
+  isDragging = false;
+  carousel2.classList.remove("dragging2");
+}
+
+const infiniteScroll2 = () => {
+  // If the carousel is at the beginning, scroll to the end
+  if(carousel2.scrollLeft === 0) {
+    carousel2.classList.add("no-transition2");
+    carousel2.scrollLeft = carousel2.scrollWidth - ( 2 * carousel2.offsetWidth);
+    carousel2.classList.remove("no-transition2");
+  } 
+  // If the carousel is at the end, scroll to the beginning
+  else if(Math.ceil(carousel2.scrollLeft) === carousel2.scrollWidth - carousel2.offsetWidth){
+    carousel2.classList.add("no-transition2");
+    carousel2.scrollLeft = carousel.offsetWidth;
+    carousel2.classList.remove("no-transition2");
+  }
+}
+carousel2.addEventListener("mousedown", dragStart2);
+carousel2.addEventListener("mousemove", dragging2);
+document.addEventListener("mouseup", dragStop2);
+carousel2.addEventListener("scroll", infiniteScroll2);
